c929a2f56bada855023d006838ff7a1609d9a7bd
Abhi <abhijithmurali2006@gmail.com>
2026-03-08 22:21:57 +0530

    Create Harshad Number.py

---
+++ Day 14/Harshad Number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
+        s, i = 0, x
+        while i:
+            s += i % 10
+            i //= 10
+        return -1 if x % s else s
