f122ebacaad49959d1378907e0a9f181e602748d
Abhi <abhijithmurali2006@gmail.com>
2026-03-15 19:22:31 +0530

    Create Detect Capital.py

---
+++ Day 21/Detect Capital.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def detectCapitalUse(self, word):
+        all_up = word.isupper()
+        all_low = word.islower()
+        title_case = word[0].isupper() and word[1:].islower()
+        
+        return all_up or all_low or title_case
